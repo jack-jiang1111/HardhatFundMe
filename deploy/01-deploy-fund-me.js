b2772b7abe2773f177f4710f2373343bd6480706
unknown <jackjiang2345@gmail.com>
2024-05-26 19:01:46 -0600

    finish harhat FundMe!

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,38 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+require("dotenv").config()
+
+// what is a mock
+// a mock is used in unit tets, used to replace complex object with a simulation behavior. For example, replace chainlink data feed with a constant
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+
+
+    // declare first
+    let ethUsdPriceFeedAddress
+
+    if(chainId == 31337){ // a local network
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    }else{
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    
+    log("----------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmations...")
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+}
+
+module.exports.tags = ["all", "fundme"]
